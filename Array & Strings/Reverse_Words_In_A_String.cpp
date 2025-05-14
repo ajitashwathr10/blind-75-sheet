661b39974d7688f6e2786add61a96d285286ba49
ajitashwathr10 <ajitashwathr23@lpu.in>
2025-05-14 21:35:43 +0530

    Create Reverse_Words_In_A_String.cpp

---
+++ Array & Strings/Reverse_Words_In_A_String.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stack<string> st;
+        string word = "";
+        for(int i = 0; i < s.length(); i++) {
+            if(s[i] == ' ') {
+                if(!word.empty()) {
+                    st.push(word);
+                    word = "";
+                }
+            } else {
+                word += s[i];
+            }
+        }
+        if(!word.empty()) st.push(word);
+        while(st.size() != 1) {
+            res += st.top() + " ";
+            st.pop();
+        }
+        res += st.top();
+        return res;
+    }
+};
